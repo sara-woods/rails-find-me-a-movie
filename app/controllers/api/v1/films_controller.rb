215e36c4a50634fa046ffa356dd91ccb21ddeea1
Sara Lotfi <lotfi.sara@gmail.com>
2021-05-03 19:21:31 +0200

    created controller and post route for film search

---
+++ app/controllers/api/v1/films_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::FilmsController < Api::V1::BaseController
+  def search
+    # https://www.imdb.com/search/title/?title_type=feature&release_date=2020-01-01,2021-01-01&user_rating=1.0,&genres=animation
+    # puts params[:url]
+  end
+
+
+end
