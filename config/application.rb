5923180c4e2800357d2612c1cc1a3fe7ac20e3bf
Sara Lotfi <lotfi.sara@gmail.com>
2021-10-08 14:52:47 +0200

    cors clean-up

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,3 @@ end
 
 Rails.application.config.hosts << "whatmovieapp.herokuapp.com"
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://saralotfi.dev'
-    resource '*', headers: :any, methods: [:get]
-  end
-end
